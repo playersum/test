c2d4fb10e292e43a936fca33e5932b6300e9db40
playersum <playersuma@gmail.com>
2021-01-14 15:01:09 +0300

    driver

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 import unittest
 import random
+import os
 
 class Register(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome("C:/WebDrivers/chromedriver.exe")
+        self.wd = webdriver.Chrome(os.getcwd()+"/chromedriver.exe")
         self.wd.maximize_window()
         self.wd.implicitly_wait(5)
     def Reg(self, name, surname, mail, phone, password, password_confirm):
